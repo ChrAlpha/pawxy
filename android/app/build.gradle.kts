b10c1bef02aec3173f34c8dbaaee7fc8bfa415f7
ChrAlpha <53332481+ChrAlpha@users.noreply.github.com>
2026-05-31 07:06:27 +0000

    chore: release 0.2.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.pawxy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.2.4"
+        versionCode = 7
+        versionName = "0.2.5"
     }
 
     sourceSets["main"].jniLibs.srcDirs("src/main/jniLibs")
